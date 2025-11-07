3b37f0ecdcc06106e0f7e38b0ac642ee4c1d5054
Sean Fan <seantosfan@gmail.com>
2025-11-07 09:40:25 -0800

    M7L9

--- M07.telling_a_story_workbook/lessons/expressions.gd
+++ M07.telling_a_story_workbook/lessons/expressions.gd
@@ -13,35 +13,43 @@ var expressions := {
 	
 @onready var body: TextureRect = %body
 @onready var expression: TextureRect = %Expression
-@onready var button_sophia: Button = %ButtonSophia
-@onready var button_pink: Button = %ButtonPink
-@onready var button_regular: Button = %ButtonRegular
-@onready var button_sad: Button = %ButtonSad
-@onready var button_happy: Button = %ButtonHappy
+
+@onready var row_bodies: HBoxContainer = $VBoxContainer/RowBodies
+@onready var row_expressions: HBoxContainer = $VBoxContainer/RowExpressions
 
 
+func create_button_pink() -> void:
+	var button := Button.new()
+	row_bodies.add_child(button)
+	
+	var key := "pink"
+	button.text = key.capitalize()
+	button.pressed.connect(func() -> void:
+		body.texture = bodies[key]
+	)
 
 
 func _ready() -> void:
-	body.texture = bodies["pink"]
-	expression.texture = expressions["happy"]
+	create_buttons()
 	
+func create_buttons() -> void:
+	for current_body: String in bodies:
+		var button := Button.new()
+		row_bodies.add_child(button)
+		button.text = current_body.capitalize()
+		button.pressed.connect(func() -> void:
+				body.texture = bodies[current_body]
+		)
 	
-	button_sophia.pressed.connect (func() -> void:
-		body.texture = bodies["sophia"])
-	
-	button_pink.pressed.connect (func() -> void:
-		body.texture = bodies["pink"])
+	for current_expression: String in expressions:
+		var button := Button.new()
+		row_expressions.add_child(button)
+		button.text = current_expression.capitalize()
+		button.pressed.connect(func() -> void:
+				expression.texture = expressions[current_expression]
+		)
 		
-	button_regular.pressed.connect (func() -> void:
-		expression.texture = expressions["regular"])
-		
-	button_sad.pressed.connect (func() -> void:
-		expression.texture = expressions["sad"])
-		
-	button_happy.pressed.connect (func() -> void:
-		expression.texture = expressions["happy"])
-		
+
 	
 	
 	
