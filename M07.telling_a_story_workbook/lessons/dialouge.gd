31550f180db7352e54a73ddd00c168d0fd2c102d
Sean Fan <seantosfan@gmail.com>
2025-11-07 15:20:28 -0800

    M7L11

--- M07.telling_a_story_workbook/lessons/dialouge.gd
+++ M07.telling_a_story_workbook/lessons/dialouge.gd
@@ -3,12 +3,33 @@ extends Control
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
 @onready var next_button: Button = %NextButton
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var body: TextureRect = %body
+@onready var expression: TextureRect = %Expression
+
+var expressions:= {
+	"happy": preload ("res://assets/emotion_happy.png"),
+	"regular": preload ("res://assets/emotion_regular.png"),
+	"sad":preload("res://assets/emotion_sad.png")
+	
+	
+}
+	
 
 
-var dialogue_items:Array[String] = [
-	"Howdy y'all, I'm Sophia", 
-	"Today I did nothing!",
-	"Favorite food is fried chicken"]
+var dialogue_items:Array[Dictionary] = [
+	
+	{
+		"expression": expressions["happy"],
+		"text":"Howdy y'all, I'm Sophia",
+		},
+	{
+		"expression": expressions["regular"],
+		"text":"Today, I did nothing!",
+		},
+	
+	{	"expression": expressions["sad"],
+		"text":"I'm unemployed", }
+	]
 	
 var current_item_index := 0	
 
@@ -16,17 +37,20 @@ var current_item_index := 0
 
 func show_text() -> void:
 	var current_item := dialogue_items[current_item_index]
-	rich_text_label.text = current_item
+	rich_text_label.text = current_item["text"]
+	expression.texture = current_item["expression"]
 	rich_text_label.visible_ratio = 0.0
 	
 	var tween := create_tween()
-	var text_appearing_duration := 1.2
+	var text_appearing_duration := 2
 	tween.tween_property	(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
 	
 	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
 	var sound_start_position := randf() * sound_max_offset
 	audio_stream_player.play(sound_start_position)
 	tween.finished.connect(audio_stream_player.stop)
+	
+	slide_in()
 	
 func advance() -> void:
 	current_item_index +=1 
@@ -38,6 +62,20 @@ func advance() -> void:
 func _ready() -> void:
 	show_text()
 	next_button.pressed.connect(advance)
+	
+	
+func slide_in()-> void:
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_QUART)
+	tween.set_ease(Tween.EASE_OUT)
+	
+	body.position.x = 100.0
+	tween.tween_property(body, "position:x", 0.0, 0.3)
+	
+	body.modulate.a = 0.0 
+	tween.parallel().tween_property(body, "modulate:a", 1.0, 0.2)
+	
+	
 	
 	
 	
